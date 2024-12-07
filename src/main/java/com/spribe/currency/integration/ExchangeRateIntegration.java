44efd598ae1543bc0b385b4b81173cff7c13e830
Roman Trukhanovich <roman.trukhanovich@gmail.com>
2024-12-07 20:35:35 +0300

    Add retry

--- src/main/java/com/spribe/currency/integration/ExchangeRateIntegration.java
+++ src/main/java/com/spribe/currency/integration/ExchangeRateIntegration.java
@@ -1,20 +1,13 @@
 package com.spribe.currency.integration;
 
 import com.spribe.currency.dto.ExchangeRateIntegrationResponse;
-import com.spribe.currency.entity.Currency;
-import com.spribe.currency.entity.ExchangeRate;
 import com.spribe.currency.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 @RequiredArgsConstructor
@@ -29,20 +22,11 @@ public class ExchangeRateIntegration {
     @Value("${integration.exchange-rate.format}")
     private int format;
 
+    @Retryable(retryFor = {RuntimeException.class}, backoff = @Backoff(delay = 2000))
     public ExchangeRateIntegrationResponse getExchangeRates(Set<String> currencies, String source) {
         try {
-//            ExchangeRateIntegrationResponse response = exchangeRateClient.getExchangeRates(
-//                    accessKey, StringUtils.joinList(currencies), source, format);
-
-            Map<String, BigDecimal> mockRates = getMockExchangeRates(currencies, source);
-            var response = new ExchangeRateIntegrationResponse(
-                    true,
-                    "terms-and-conditions-url",
-                    "privacy-policy-url",
-                    System.currentTimeMillis(),
-                    source,
-                    mockRates
-            );
+            ExchangeRateIntegrationResponse response = exchangeRateClient.getExchangeRates(
+                    accessKey, StringUtils.joinList(currencies), source, format);
 
             if (response != null && response.success()) {
                 return response;
@@ -52,17 +36,5 @@ public class ExchangeRateIntegration {
         } catch (Exception e) {
             throw new RuntimeException("An error occurred while fetching exchange rates", e);
         }
-    }
-
-    private Map<String, BigDecimal> getMockExchangeRates(Set<String> currencies, String source) {
-        Map<String, BigDecimal> exchangeRates = new HashMap<>();
-
-        for (String targetCurrency : currencies) {
-            String currencyPair = source + targetCurrency;
-            BigDecimal rate = BigDecimal.valueOf(Math.random() * 100);
-            exchangeRates.put(currencyPair, rate);
-        }
-
-        return exchangeRates;
     }
 }
